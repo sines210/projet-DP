b3017e2ba0dc1f8c5e946f194971b4fa548895f5
sines210 <sines210@gmail.com>
2021-04-19 20:37:32 +0200

    env

--- bd/connexionDB.php
+++ bd/connexionDB.php
@@ -2,12 +2,15 @@
 
 
 
+$userName = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
+$dsn =  $_ENV['DSN'];
 
 
 
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbName", $userName, $pass);
+    $db = new PDO($dsn, $userName, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage();
