51c3128e91943bed9a0384327d48047894d3d257
sines210 <sines210@gmail.com>
2021-04-23 11:38:17 +0200

    csp-comments

--- index.php
+++ index.php
@@ -24,21 +24,26 @@
 
 <?php
 header("X-XSS-Protection:1; mode=block");
+//ce header protège des failles XSS des infos envoyées dans les header http via Internet Explorer, Chrome et Safari, le 1 dit que la protection est active et le bloxk que la réponse soit bloquée si une attaque est détectée
 header("X-Frame-Options:SAMEORIGIN"); 
+// Potège des attaques de type 'clickjacking' 
+//X-Frame-Options:SAMEORIGIN - This means that the page can only be embedded in a frame on a page with the same origin as itself. 
 header("X-Content-Type-Options:nosniff");
+//parade aux attaques MIME
+//attaques MIME => MIME sniffing vulnerabilities can occur when a website allows users to upload data to the server. The vulnerability comes into play when an attacker disguises an HTML file as a different file type (e.g. a JPEG, zip file, etc.). ..
+// With the nosniff option, if the server says the content is text/html, the browser will render it as text/html.
 header("Strict-Transport-Security:max-age=31536000; includeSubdomains; preload");
 // header("Referrer-Policy : no-referrer-when-downgrade");
 // header("Content-Securiy-Policy:upgrade-insecure-requests");
 
-@ini_set("session.cookie_httponly", true);
-@ini_set("session.cookie_secure", true);
+// @ini_set("session.cookie_httponly", true);
+// @ini_set("session.cookie_secure", true);
 // @ini_set("session.use_only_cookies", true);
 
 ?>
 
 
 </head>
-
 
 
 <body>
