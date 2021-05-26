955df5ef6e0b850564332945cf55994254a5b084
sines210 <sines210@gmail.com>
2021-05-26 23:05:42 +0200

    gmail server

--- config/config_contact.php
+++ config/config_contact.php
@@ -38,16 +38,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.mailgun.org';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'postmaster@sandbox9193718121d643d0b8410437d660181b.mailgun.org';                     //SMTP username
-    $mail->Password   = '100d23b531097a892827522fce69d948-fa6e84b7-69d2fdab';  //SMTP password
+    $mail->Username   = 'casperSouidi@gmail.com';                     //SMTP username
+    $mail->Password   = 'casperSouidi13';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('postmaster@sandbox9193718121d643d0b8410437d660181b.mailgun.org', 'mailer');
-    // $mail->setFrom('casperSouidi@gmail.com', 'mailer');
+    $mail->setFrom('casperSouidi@gmail.com', 'mailer');
     $mail->addAddress('casperSouidi@gmail.com', 'Admin');     //Add a recipient
  
 
